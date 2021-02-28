c1fceb8f038da062737e177e236c4291aa09925a
AndreyErmolkin <78263246+AndreyErmolkin@users.noreply.github.com>
2021-02-28 20:16:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HomeWork_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            Console.ReadLine();
+            int N = int.Parse(Console.ReadLine());
+            
+
+            for (int i = 0; i <= N; i++)
+                if (Simple(i))
+                {
+                    Console.WriteLine(i.ToString() + "\n");
+                }
+        }
+
+        private static bool Simple(int N)
+        {
+            for (int i = 2; i <= N / 2; i++)
+                if (N % i == 0)
+                {
+                    return false;
+                    
+                }
+                else return true;
+            
+        }
+
+    }
+}
